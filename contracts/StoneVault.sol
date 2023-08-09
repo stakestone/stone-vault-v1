b998136d228acb4d128f6255f445029974ac2576
luoyhang003 <luoyhang003@Hotmail.com>
2023-08-10 01:57:24 +0800

    fix: STS-55

--- contracts/StoneVault.sol
+++ contracts/StoneVault.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.7;
+pragma solidity 0.8.21;
 
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
--- contracts/strategies/StrategyController.sol
+++ contracts/strategies/StrategyController.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.7;
+pragma solidity 0.8.21;
 
 import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import {TransferHelper} from "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
