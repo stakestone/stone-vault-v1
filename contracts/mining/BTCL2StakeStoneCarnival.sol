dd21c2e43bf5c4a4044b24430915ad331a90fb59
luoyhang003 <luoyhang003@Hotmail.com>
2024-04-06 12:30:00 +0800

    fix: typo

--- contracts/mining/BTCL2StakeStoneCarnival.sol
+++ contracts/mining/BTCL2StakeStoneCarnival.sol
@@ -156,7 +156,7 @@ contract BTCL2StakeStoneCarnival is Ownable2Step {
     }
 
     function setAddrs(address _vaultAddr, address _lpAddr) external onlyOwner {
-        require(!isExec, "not exec");
+        require(!isExec, "already exec");
 
         vaultAddr = _vaultAddr;
         lpAddr = _lpAddr;
