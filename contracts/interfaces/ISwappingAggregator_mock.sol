0d1e0102046924a8a7a9601c82330c10ffbf4617
tomorrowsuper <tomorrowsuper@outlook.com>
2024-04-24 10:39:06 +0800

    test

--- contracts/interfaces/ISwappingAggregator_mock.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.21;
-
-// 定义 SwappingAggregator 接口
-interface ISwappingAggregator_mock {
-    function swap(address _token, uint256 _amount, bool _isSell) external returns (uint256);
-}
