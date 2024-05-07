d9b30f397efd86e45520ab97faabf22a7a671f28
luoyhang003 <luoyhang003@Hotmail.com>
2024-05-07 18:23:54 +0800

    remove: remove Eigen restaking related params.

--- contracts/strategies/eigen/Account.sol
+++ contracts/strategies/eigen/Account.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
-import {EigenNativeRestakingStrategy} from "./EigenNativeRestakingStrategy.sol";
+import {EigenStrategy} from "../EigenStrategy.sol";
 
 import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
 
@@ -16,11 +16,11 @@ contract Account is Ownable2Step {
     constructor(address _admin) {
         require(_admin != address(0), "invalid admin");
 
-        controller = msg.sender;
+        controller = EigenStrategy(address(this)).controller();
     }
 
     modifier onlyAuth() {
-        require(msg.sender == controller || msg.sender == owner(), "unauth");
+        require(msg.sender == owner(), "unauth");
         _;
     }
 
@@ -34,16 +34,7 @@ contract Account is Ownable2Step {
         uint256 value,
         bytes memory data
     ) public onlyAuth returns (bytes memory result) {
-        EigenNativeRestakingStrategy strategy = EigenNativeRestakingStrategy(
-            payable(controller)
-        );
-
-        require(
-            value == 0 ||
-                target == controller ||
-                target == strategy.batchDeposit(),
-            "not permit"
-        );
+        require(value == 0 || target == controller, "not permit");
 
         bool success;
         (success, result) = target.call{value: value}(data);
